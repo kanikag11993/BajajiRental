ed676f486565e0e1a7a49497a298e08d35f1b400
kanika <kangupta@amazon.com>
2020-04-11 00:05:16 +0530

    Changed to main activity for running mutation of test table

--- app/src/main/java/com/balaji/rental/MainActivity.java
+++ app/src/main/java/com/balaji/rental/MainActivity.java
@@ -6,15 +6,20 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amazonaws.amplify.generated.graphql.CreatePropertyMutation;
+import com.amazonaws.amplify.generated.graphql.CreatePropertyTestMutation;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.apollographql.apollo.GraphQLCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 
+import java.util.UUID;
+
 import javax.annotation.Nonnull;
 
 import type.CreatePropertyInput;
+import type.CreatePropertyTestInput;
+import type.Status;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 .awsConfiguration(new AWSConfiguration(getApplicationContext()))
                 .build();
         runMutation();
+        runMutationTest();
     }
 
     public void runMutation(){
@@ -44,6 +50,36 @@ public class MainActivity extends AppCompatActivity {
     private GraphQLCall.Callback<CreatePropertyMutation.Data> mutationCallback = new GraphQLCall.Callback<CreatePropertyMutation.Data>() {
         @Override
         public void onResponse(@Nonnull Response<CreatePropertyMutation.Data> response) {
+            Log.i("Results", "Added Todo");
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e("Error", e.toString());
+        }
+    };
+
+    public void runMutationTest(){
+        CreatePropertyTestInput createPropertyTestInput = CreatePropertyTestInput.builder()
+                .owner("Rajender")
+                .flatName(UUID.randomUUID().toString())
+                .address("Dummy")
+                .currentMonthElectricMeterReading(120)
+                .previousMonthElectricMeterReading(111)
+                .electricityPerUnitCost(9.5)
+                .monthlyRent("15000")
+                .status(Status.VACANT)
+                .tenantName("Dummy tenant")
+                .waterCost(180.7)
+                .build();
+
+        mAWSAppSyncClient.mutate(CreatePropertyTestMutation.builder().input(createPropertyTestInput).build())
+                .enqueue(mutationCallbackTest);
+    }
+
+    private GraphQLCall.Callback<CreatePropertyTestMutation.Data> mutationCallbackTest = new GraphQLCall.Callback<CreatePropertyTestMutation.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<CreatePropertyTestMutation.Data> response) {
             Log.i("Results", "Added Todo");
         }
 
